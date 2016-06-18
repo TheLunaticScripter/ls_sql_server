8ebd338b6aeff9556de6aba2e4751abcaad10143
John Snow <thelunaticscripter@outlook.com>
2016-06-18 13:34:53 -0400

    Corrected Ruby syntax.

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,10 +6,10 @@ default['ls_sql_server']['sql_admin_group_member'] = "$env:USERNAME"
 default['ls_sql_server']['sql_admin_group_ou'] = "OU=TheWall,DC=thewall,DC=local"
 default['ls_sql_server']['sql_account'] = 'THEWALL.LOCAL\\sql.service'
 default['ls_sql_server']['sql_source'] = 'c:\\Sources\\SQL2012SP3'
-default['ls_sql_server']['sysadmins'] = "THEWALL.LOCAL\\SQL Administrators"
-default['ls_sql_server']['agent_account_pwd'] = '!QAZSE$1qazse4'
-default['ls_sql_server']['sql_account_pwd'] = '!QAZSE$1qazse4'
+default['ls_sql_server']['sysadmins'] = "THEWALL.LOCAL\\SQL Admins"
+default['ls_sql_server']['agent_account_pwd'] = 'super_secret_password'
+default['ls_sql_server']['sql_account_pwd'] = 'super_secret_password'
 default['ls_sql_server']['cluster'] = "standalone"
 default['ls_windows_cluster']['cluster_name'] = "SQLtestCluster"
-default['ls_windows_cluster']['cluster_ip_address'] = '10.0.3.232'
+default['ls_windows_cluster']['cluster_ip_address'] = '10.0.0.3'
 
